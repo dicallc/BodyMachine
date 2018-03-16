5cbacccea2ae44375cf0200076525505fb5f5e9f
jiangdk <jiangdk@e-lead.cn>
2018-03-16 18:17:19 +0800

    test push

---
+++ app/src/main/java/com/fliggy/bodymachine/utils/Constant.java
@@ -0,0 +1,21 @@
+package com.fliggy.bodymachine.utils;
+
+/**
+ * Created by dicallc on 2018/3/16.
+ */
+
+public class Constant {
+  public static final String Wt="Wt";
+  public static final String FatWt="FatWt";
+  public static final String MuscleWt="MuscleWt";
+  public static final String ToatalWatWt="ToatalWatWt";
+  public static final String FatFreeBodyWt="FatFreeBodyWt";
+  public static final String BoneSalt="BoneSalt";
+  public static final String Protein="Protein";
+  public static final String BMI="BMI";
+  public static final String PBF="PBF";
+  public static final String PBW="PBW";
+  public static final String PhysicalAge="PhysicalAge";
+  public static final String PhysicalScore="PhysicalScore";
+  public static final String ObesityDegree="ObesityDegree";
+}
